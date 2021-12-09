0f7e76f44e8e3760a39ee5dc5c278a6d9d00af7a
Chistyakov Daniil <danyaalok@mail.ru>
2021-12-09 18:54:15 +0300

    16

---
+++ src/ru/mirea/task26/Main.java
@@ -0,0 +1,16 @@
+package ru.mirea.task26;
+
+interface Payment { public void pay(); }
+
+class Store {
+    public void start() {
+
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Store st = new Store();
+        st.start();
+    }
+}
